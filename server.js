654b44c91225abc4e1dfd8eff94efd91c8312a08
Filip Frysz <ffrysz@gmail.com>
2021-11-22 20:03:18 +0100

    Add all templates

--- server.js
+++ server.js
@@ -5,44 +5,48 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('.hbs', hbs());
+app.engine('.hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (file) => {
-    res.sendFile(path.join(__dirname, `/views/${file}`));
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   res.show = (file) => {
+//     res.sendFile(path.join(__dirname, `/views/${file}`));
+//   };
+//   next();
+// });
 
 app.use('/user/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/views/forbidden.html'));
+  res.render('forbidden', { layout: false });
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get(['/', '/home'], (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
-
-// app.get('/hello/:name', (req, res) => {
-//   res.send(`<h1>Hello ${req.params.name}</h1>`);
-// });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
-// app.get('public/404.jpg', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/404.jpg'));
-// });
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  res.status(404).render('404', { layout: false });
 })
 
 app.listen(8000, () => {
